e528d10b5e48cfbf2f449d81c1373102ee0738c7
csat1763 <aspir@live.de>
2018-10-29 15:47:32 +0100

    added named graph

--- code/TripleStore/Triple/src/main/java/org/apache/jena/example/helloworld/HelloWorld2.java
+++ code/TripleStore/Triple/src/main/java/org/apache/jena/example/helloworld/HelloWorld2.java
@@ -20,6 +20,7 @@ package org.apache.jena.example.helloworld;
 
 import org.apache.jena.example.RecipeBase;
 import org.apache.jena.ontology.OntModel;
+import org.apache.jena.query.Dataset;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
@@ -28,6 +29,7 @@ import org.apache.jena.query.ResultSet;
 import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.sparql.core.DatasetImpl;
 import org.apache.jena.util.FileManager;
 import org.apache.jena.vocabulary.OWL;
 import org.apache.jena.vocabulary.RDF;
@@ -57,7 +59,7 @@ public class HelloWorld2 extends RecipeBase {
 	public static String prefix = "prefix schema: <http://schema.org/>\n" + "prefix rdfs: <" + RDFS.getURI() + ">\n"
 			+ "prefix rdf: <" + RDF.getURI() + ">\n" + "prefix owl: <" + OWL.getURI() + ">\n";
 
-	public static Model model;
+	public static DatasetImpl model;
 
 	/***********************************/
 	/* Instance variables */
@@ -80,16 +82,21 @@ public class HelloWorld2 extends RecipeBase {
 		new HelloWorld2().setArgs(args).run();
 	}
 
+	@Override
 	public void run() {
 		// creates a new, empty in-memory model
-		model = ModelFactory.createDefaultModel();
+		Model m = ModelFactory.createDefaultModel();
 
 		OntModel o = ModelFactory.createOntologyModel();
 
 		FileManager.get().readModel(o, RECIPE_SCHEMA_FILE);
 
 		// load some data into the model
-		FileManager.get().readModel(model, RECIPE_DATA_FILE);
+		FileManager.get().readModel(m, RECIPE_DATA_FILE);
+
+		DatasetImpl ds = new DatasetImpl(m);
+		ds.addNamedModel("steve", o);
+		model = ds;
 
 		// generate some output
 
@@ -105,18 +112,21 @@ public class HelloWorld2 extends RecipeBase {
 
 		// numberOfTriples();
 		// numberOfTriplesPerClass();
-		// numberOfDistinctClasses();
+		numberOfDistinctClasses();
 		// numberOfDistinctProperties();
 		// classesPerDataSet();
 
-		showQuery(model, prefix + " SELECT ?entity ?type\r\n"
-				+ "WHERE { ?entity rdf:type/rdfs:subClassOf* schema:Thing:Property }");
+		// showQuery(model, prefix + "SELECT ?y \r\n WHERE { ?x ?y ?s . ?x a
+		// schema:Recipe} GROUP BY ?y");
 
-		showQuery(model,
-				prefix + "SELECT DISTINCT ?type\r\n" + "WHERE {\r\n" + "  ?s schema:Thing:Property ?type.\r\n" + "}");
+		// showQuery(model, prefix + "SELECT DISTINCT ?s\r\n" + "WHERE {\r\n" + " ?s a
+		// rdfs:Property\r\n" + "}");
 
-		showQuery(model, prefix + "SELECT ?x WHERE {\r\n"
-				+ "  ?x a schema:recipeIngredient . ?x schema:authot \"John Smith\" \r\n" + "}");
+		// showQuery(ds, "SELECT *\r\n" + "{\r\n" + " { ?s ?p ?o } UNION { GRAPH ?g { ?s
+		// ?p ?o } }\r\n" + "}");
+
+		// PrintUtil.printOut(ds.asDatasetGraph().find());
+
 	}
 
 	/***********************************/
@@ -127,21 +137,25 @@ public class HelloWorld2 extends RecipeBase {
 	 * Show the size of the model on stdout
 	 */
 	public static void numberOfTriples() {
-		showQuery(model, "SELECT (COUNT(?x) as ?triples)	\r\n" + "WHERE { ?x ?y ?s}");
+		showQuery(model, prefix + "SELECT (COUNT(?x) as ?triples)	\r\n" + "WHERE { ?x ?y ?s . ?x a schema:Recipe}");
 	}
 
 	public static void numberOfTriplesPerClass() {
-		showQuery(model, "SELECT  ?class (COUNT(?x) as ?count)	\r\n" + "WHERE { ?x ?class ?s} GROUP BY ?class ");
+		showQuery(model, prefix + "SELECT  ?class (COUNT(?x) as ?count)	\r\n"
+				+ "WHERE { ?x ?class ?s . ?x a schema:Recipe} GROUP BY ?class ");
 	}
 
 	public static void numberOfDistinctClasses() {
-		showQuery(model, "SELECT (COUNT(*) as ?numberOfDistinctClasses) \r\n WHERE{"
-				+ "SELECT (COUNT(distinct ?y) as ?xcount) \r\n" + "WHERE { ?x ?y ?s} GROUP BY ?y}");
+		showQuery(model,
+				prefix + "SELECT (COUNT(*) as ?numberOfDistinctClasses) \r\n WHERE{"
+						+ "SELECT (COUNT(distinct ?y) as ?xcount) \r\n"
+						+ "WHERE { ?x ?y ?s . ?x a schema:Recipe} GROUP BY ?y}");
 	}
 
 	public static void numberOfDistinctProperties() {
-		showQuery(model,
-				"SELECT  ?class (COUNT(distinct ?s) as ?count)	\r\n" + "WHERE { ?x ?class ?s} GROUP BY ?class ");
+		showQuery(model, prefix
+				+ "SELECT (COUNT(*) as ?numberOfDistinctProperties) WHERE{ SELECT DISTINCT ?Properties WHERE {\r\n"
+				+ " ?x ?Properties ?z. ?Properties a rdf:Property . ?x a schema:Recipe }GROUP BY ?Properties} ");
 	}
 
 	public static void classesPerDataSet() {
@@ -155,6 +169,18 @@ public class HelloWorld2 extends RecipeBase {
 		try {
 			ResultSet results = qexec.execSelect();
 			ResultSetFormatter.out(results, m);
+		} finally {
+			qexec.close();
+		}
+
+	}
+
+	public static void showQuery(Dataset m, String q) {
+		Query query = QueryFactory.create(q);
+		QueryExecution qexec = QueryExecutionFactory.create(query, m);
+		try {
+			ResultSet results = qexec.execSelect();
+			ResultSetFormatter.out(results);
 		} finally {
 			qexec.close();
 		}
