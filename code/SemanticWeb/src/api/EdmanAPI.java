68dce66629e387c9be31ecf2fec645e3ee1c8f26
csat1763 <aspir@live.de>
2018-11-02 18:49:16 +0100

    added crawler for edamam

--- code/SemanticWeb/src/api/EdmanAPI.java
+++ code/SemanticWeb/src/api/EdmanAPI.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,10 +12,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -25,10 +32,49 @@ import com.google.gson.internal.LinkedTreeMap;
 
 public class EdmanAPI {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		getData();
+	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+		// getData();
 		// translateJsonLdToN3();
 		// translateTxtToJson();
+		crawl();
+
+	}
+
+	public static void crawl() throws InterruptedException, FileNotFoundException, ExecutionException {
+		ExecutorService pool = Executors.newFixedThreadPool(5);
+		ArrayList<Future<String>> results = new ArrayList<Future<String>>();
+		int i = 0;
+		boolean endNotReached = true;
+		while (endNotReached) {
+			results.add(pool.submit(new EdamamCrawler(i, i + 100)));
+			results.add(pool.submit(new EdamamCrawler(i + 100, i + 200)));
+			results.add(pool.submit(new EdamamCrawler(i + 200, i + 300)));
+			results.add(pool.submit(new EdamamCrawler(i + 300, i + 400)));
+			results.add(pool.submit(new EdamamCrawler(i + 400, i + 500)));
+			i += 500;
+
+			for (Future<String> res : results) {
+				try {
+					if (res.get() == null) {
+						endNotReached = false;
+					}
+				} catch (ExecutionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+		File recipesFromEdamam = new File("omgLuL.jsonld");
+
+		PrintWriter tempWriter = new PrintWriter(recipesFromEdamam);
+
+		for (Future<String> res : results) {
+			tempWriter.print(res.get());
+			tempWriter.flush();
+		}
+
+		tempWriter.close();
 
 	}
 
@@ -250,4 +296,127 @@ public class EdmanAPI {
 
 	}
 
+}
+
+class EdamamCrawler implements Callable<String> {
+
+	private int from;
+	private int to;
+
+	public EdamamCrawler(int from, int to) {
+		this.from = from;
+		this.to = to;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public String call() {
+		URL url;
+		try {
+			url = new URL(
+					"https://api.edamam.com/search?q=pasta&app_id=6362f010&app_key=0a2cfb0cce312b298bf239c7c37790a8&from="
+							+ from + "&to=" + to);
+
+			// Create instance of connection to the API URL
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+
+			// We will get the result in json format
+			conn.setRequestProperty("Accept", "application/json");
+			conn.setDoOutput(true);
+			// Read response body from the stream returned by getInputStream()
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream()), "UTF-8"));
+
+			StringBuilder rep = new StringBuilder();
+			String output = "";
+			while ((output = br.readLine()) != null) {
+				rep.append(output);
+			}
+
+			File recipesFromTxt = new File("edamamResult" + from + "-" + to + ".jsonld");
+
+			PrintWriter tempWriter = new PrintWriter(recipesFromTxt);
+			tempWriter.print(rep.toString());
+			tempWriter.flush();
+			tempWriter.close();
+
+			// Transform output to json
+			LinkedTreeMap<String, Object> jsonResult = new Gson().fromJson(rep.toString(), LinkedTreeMap.class);
+
+			List<LinkedTreeMap<String, Object>> hits = (ArrayList<LinkedTreeMap<String, Object>>) jsonResult
+					.get("hits");
+
+			if (null != hits && !hits.isEmpty()) {
+
+				StringBuilder recipiesAsString = new StringBuilder();
+				System.out.println("Entering result!");
+				// recipiesAsString.append("[\n");
+				for (LinkedTreeMap<String, Object> hit : hits) {
+
+					StringBuilder ingredientsAsString = new StringBuilder();
+
+					// Publishing Date (YYYY-MM-DD) begin
+					LinkedTreeMap<String, Object> recipe = (LinkedTreeMap<String, Object>) hit.get("recipe");
+
+					String uri, label, recipeUrl, imageUrl;
+					List<String> ingredients = new ArrayList<String>();
+					double calories, yield, totalTime;
+					// TODO: totalNutrients, healthLabels, source(author)
+
+					uri = (String) recipe.get("uri");
+					label = (String) recipe.get("label");
+					recipeUrl = (String) recipe.get("url");
+					imageUrl = (String) recipe.get("image");
+					yield = (double) recipe.get("yield");
+					calories = (double) recipe.get("calories");
+					totalTime = (double) recipe.get("totalTime");
+					calories = Math.round(calories);
+
+					ingredients = (ArrayList<String>) recipe.get("ingredientLines");
+
+					/*
+					 * System.out.println("\nnew entry:\n" + " uri: " + uri + "\nlabel: " + label + "\nurl: " + recipeUrl + "\ncalories: " + calories + "\ningredients" + ingredients);
+					 */
+
+					ingredientsAsString.append("[\n");
+
+					for (String i : ingredients) {
+						ingredientsAsString.append("\t\t\"").append(i).append("\",\n");
+					}
+					ingredientsAsString.delete(ingredientsAsString.length() - 2, ingredientsAsString.length() - 1);
+
+					ingredientsAsString.append("\t]");
+
+					// System.out.println("\njson-ld:\n");
+
+					recipiesAsString.append("{\n" + "\t\"@context\": \"http://schema.org\",\n"
+							+ "\t\"@type\": \"Recipe\",\n" + "\t\"author\": \"John Smith\", \n" + "\t\"name\": \""
+							+ label + "\",\n" + "\t\"identifier\": \"" + uri + "\",\n" + "\t\"url\": \"" + recipeUrl
+							+ "\",\n" + "\t\"image\": \"" + imageUrl + "\",\n" + "\t\"recipeYield\": \"" + yield
+							+ "\",\n" + "\t\"totalTime\": \"" + totalTime + "\",\n"
+							+ "\t\"nutrition\": {\n\t\t\"@type\": \"NutritionInformation\",\n\t\t\"calories\" : \""
+							+ calories + " calories\"\n\t},\n" + "\t\"recipeIngredient\": "
+							+ ingredientsAsString.toString() + " \n" + "},\n");
+
+				}
+
+				// recipiesAsString.delete(recipiesAsString.length() - 2, recipiesAsString.length() - 1);
+				// recipiesAsString.append("]");
+
+				conn.disconnect();
+				System.out.println("New chunk added!");
+				return recipiesAsString.toString();
+
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("failed to add!");
+		return null;
+
+	}
 }
