dc8e1551eb513de5ccdbe76ee55f10acd3f5d9d3
csat1763 <aspir@live.de>
2019-01-23 20:10:50 +0100

    added queries for ing+keywords

---
+++ code/BackEnd/src/main/java/fuseki/FusekiLoader.java
@@ -0,0 +1,22 @@
+
+package fuseki;
+
+import org.apache.jena.sparql.core.DatasetImpl;
+
+public class FusekiLoader {
+
+	public static String dataSet = "..\\TripleStore\\Triple\\src\\main\\resources\\data\\recipes";
+	public static String ontology = "..\\TripleStore\\Triple\\src\\main\\resources\\ontologies\\currentOntology";
+	public static String datasetName = "food";
+
+	public static DatasetImpl model;
+
+	public static void main(String[] args) {
+		FusekiConnection fc = new FusekiConnection("http://localhost:3030");
+		fc.dataSet = dataSet;
+		fc.ontology = ontology;
+		fc.datasetName = datasetName;
+		fc.loadData();
+	}
+
+}
--- code/BackEnd/src/main/java/recipeBackend/FusekiEndpoint.java
+++ code/BackEnd/src/main/java/recipeBackend/FusekiEndpoint.java
@@ -1,9 +1,69 @@
 package recipeBackend;
 
-import helloworld.FusekiConnection;
+import java.util.ArrayList;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
+import fuseki.FusekiConnection;
+
+/* Fuseki must be running and must have at least one dataset in graph <http://localhost:3030/food/data/data> 
+ * and an ontology in graph <http://localhost:3030/food/data/ontology>
+ * */
 public class FusekiEndpoint {
 
-	FusekiConnection fc = new FusekiConnection("http://localhost:3030");
+	public static String fusekiAdr = "http://localhost:3030";
+	public static String frontEntAdr = "https://httpbin.org/post";
+	public static String fusekiDataGraphName = "<http://localhost:3030/food/data/data>";
+	public static String fusekiOntGraphName = "<http://localhost:3030/food/data/ontology>";
+	public static String prefix = "PREFIX schema: <http://schema.org/>\r\n"
+			+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\r\n"
+			+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\r\n"
+			+ "PREFIX owl: <http://www.w3.org/2002/07/owl#>\r\n";
+
+	public static void main(String args[]) {
+		runExample();
+	}
+
+	public static void runExample() {
+		FusekiConnection fc = new FusekiConnection(fusekiAdr);
+		ArrayList<String> ings = new ArrayList<String>();
+		ings.add("salt");
+		ings.add("pepper");
+		ings.add("butter");
+		ArrayList<String> keys = new ArrayList<String>();
+		keys.add("Alcohol-Free");
+		// System.out.println(query(ings, keys));
+		fc.sendQueryResultsToAdr(generateQuery(ings, keys), frontEntAdr);
+	}
+
+	public static String generateQuery(ArrayList<String> ings, ArrayList<String> keywords) {
+		StringBuilder query = new StringBuilder();
+		query.append(prefix)
+				.append("DESCRIBE ?s " + "FROM " + fusekiOntGraphName + " \r\n" + "FROM " + fusekiDataGraphName
+						+ " \r\n" + "{ SELECT \r\n" + "DISTINCT ?s \r\n" + "WHERE{")
+				.append("\r\n?s ?p ?o . \r\n" + "?s a schema:Recipe . ");
+		if (ings != null && !ings.isEmpty()) {
+			for (String inge : ings) {
+				String ing = DigestUtils.sha256Hex(inge);
+				query.append("?s schema:recipeIngredient ?" + ing + ".\r\n")
+						.append("?" + ing + " schema:ingredientName ?" + ing + "1 .\r\n")
+						.append("?" + ing + "1 schema:name ?" + ing + "2 .\r\n")
+						.append("FILTER regex(?" + ing + "2,\"" + inge + "\") .");
+
+			}
+		} else {
+			return null;
+		}
+		if (keywords != null && !keywords.isEmpty()) {
+			for (String key : keywords) {
+				String keyw = DigestUtils.sha256Hex(key);
+				query.append("?s schema:keywords ?" + keyw + ".\r\n")
+						.append("FILTER regex(?" + keyw + ",\"" + key + "\") .\r\n");
+
+			}
+		}
+		query.append("  \r\n" + "  } LIMIT 5}");
+		return query.toString();
+	}
 
 }
