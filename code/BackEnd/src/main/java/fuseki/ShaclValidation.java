41e6515d1e61a7f38d5f361b12d126e9c835b918
csat1763 <aspir@live.de>
2019-01-29 15:09:56 +0100

    shacl validator automized

--- code/BackEnd/src/main/java/fuseki/ShaclValidation.java
+++ code/BackEnd/src/main/java/fuseki/ShaclValidation.java
@@ -1,10 +1,20 @@
 package fuseki;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Resource;
@@ -18,6 +28,10 @@ import org.topbraid.shacl.validation.ValidationUtil;
 import org.topbraid.shacl.vocabulary.SH;
 import org.topbraid.spin.util.JenaUtil;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.internal.LinkedTreeMap;
+
 public class ShaclValidation {
 	private static Logger logger = LoggerFactory.getLogger(ShaclValidation.class);
 
@@ -29,40 +43,125 @@ public class ShaclValidation {
 		try {
 			Path path = Paths.get(".").toAbsolutePath().normalize();
 
-			// TODO: do for all recipes
-			for (String filenamex : FusekiConnection
+			for (String data : FusekiConnection
 					.listFilesForFolder(new File("../TripleStore/Triple/src/main/resources/data/recipes"))) {
-				System.out.println(filenamex);
-			}
-			// TODO: you mean like this?
-			String data = "file:" + path.toFile().getAbsolutePath()
-					+ "/src/main/resources/shacl/###TODO###FromEdamam.jsonld";
-			String shape = "file:" + path.toFile().getAbsolutePath() + "/src/main/resources/shacl/shacl.ttl";
+				String cuisineName = getCuisineFromFile(data);
 
-			Model dataModel = JenaUtil.createDefaultModel();
-			dataModel.read(data);
+				String shape = "file:" + path.toFile().getAbsolutePath() + "/src/main/resources/shacl/shacl.ttl";
 
-			Model shapeModel = JenaUtil.createDefaultModel();
-			shapeModel.read(shape);
+				Model dataModel = JenaUtil.createDefaultModel();
+				dataModel.read(data);
 
-			Resource reportResource = ValidationUtil.validateModel(dataModel, shapeModel, true);
-			boolean conforms = reportResource.getProperty(SH.conforms).getBoolean();
-			logger.trace("Conforms = " + conforms);
+				Model shapeModel = JenaUtil.createDefaultModel();
+				shapeModel.read(shape);
 
-			if (!conforms) {
-				String report = path.toFile().getAbsolutePath() + "/src/main/resources/shacl/" + data + "report.ttl";
-				File reportFile = new File(report);
+				Resource reportResource = ValidationUtil.validateModel(dataModel, shapeModel, true);
+				boolean conforms = reportResource.getProperty(SH.conforms).getBoolean();
+				logger.trace("Conforms = " + conforms);
 
-				reportFile.createNewFile();
-				OutputStream reportOutputStream = new FileOutputStream(reportFile);
-				RDFDataMgr.write(reportOutputStream, reportResource.getModel(), RDFFormat.TURTLE);
-				System.out.println("ERROR - Created file report.ttl in path: " + report);
-			} else {
-				System.out.println("No Shape violations detected!");
+				if (!conforms) {
+					String report = "../TripleStore/Triple/src/main/resources/shaclReports/" + cuisineName
+							+ "FromEdamamReport.jsonld";
+					File reportFile = new File(report);
+
+					reportFile.createNewFile();
+					OutputStream reportOutputStream = new FileOutputStream(reportFile);
+					RDFDataMgr.write(reportOutputStream, reportResource.getModel(), RDFFormat.JSONLD);
+					System.out.println("ERROR - Created file report.jsonld in path: " + report);
+					removeCorruptEntriesFromFile(data, report, cuisineName);
+				} else {
+					System.out.println("No Shape violations detected!");
+				}
+
 			}
 		} catch (Throwable t) {
 			logger.error(WTF_MARKER, t.getMessage(), t);
 		}
 
 	}
+
+	@SuppressWarnings({ "unchecked" })
+	public static String getCuisineFromFile(String filePath) {
+		try {
+			File rawData = new File(filePath);
+			InputStream rawDataStream;
+
+			rawDataStream = new FileInputStream(rawData);
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(rawDataStream, "UTF-8"));
+			StringBuilder rep = new StringBuilder();
+			String output = "";
+			while ((output = br.readLine()) != null) {
+				rep.append(output);
+			}
+			br.close();
+
+			ArrayList<LinkedTreeMap<String, Object>> jsonResult = new Gson().fromJson(rep.toString(), ArrayList.class);
+
+			return URLEncoder.encode(jsonResult.get(0).get("fo:cuisine").toString(), "UTF-8");
+		} catch (
+
+		IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@SuppressWarnings({ "unchecked" })
+	public static void removeCorruptEntriesFromFile(String dataFileName, String reportFileName, String cuisine) {
+		try {
+			/* Read datafile first */
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(new FileInputStream(new File(dataFileName)), "UTF-8"));
+			StringBuilder rep = new StringBuilder();
+			String output = "";
+			while ((output = br.readLine()) != null) {
+				rep.append(output);
+			}
+			br.close();
+
+			HashMap<String, LinkedTreeMap<String, Object>> idToIdx = new HashMap<String, LinkedTreeMap<String, Object>>();
+
+			/* Then reportfile */
+			ArrayList<LinkedTreeMap<String, Object>> jsonResult = new Gson().fromJson(rep.toString(), ArrayList.class);
+			for (LinkedTreeMap<String, Object> recipeEntry : jsonResult) {
+				idToIdx.put((String) recipeEntry.get("@id"), recipeEntry);
+			}
+
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(reportFileName)), "UTF-8"));
+			rep = new StringBuilder();
+			output = "";
+			while ((output = br.readLine()) != null) {
+				rep.append(output);
+			}
+			br.close();
+
+			ArrayList<LinkedTreeMap<String, Object>> reportEntries = (ArrayList<LinkedTreeMap<String, Object>>) new Gson()
+					.fromJson(rep.toString(), LinkedTreeMap.class).get("@graph");
+
+			for (LinkedTreeMap<String, Object> reportEntry : reportEntries) {
+				if (reportEntry.get("@type").equals("http://www.w3.org/ns/shacl#ValidationResult")) {
+					String id = (String) reportEntry.get("focusNode");
+					jsonResult.remove(idToIdx.get(id));
+				}
+			}
+
+			try (OutputStreamWriter writer = new OutputStreamWriter(
+					new FileOutputStream(new File("../TripleStore/Triple/src/main/resources/data/recipes/" + cuisine
+							+ "FromEdamamSHACLvalid.jsonld")),
+					StandardCharsets.UTF_8)) {
+				writer.write(new GsonBuilder().setPrettyPrinting().create().toJson(jsonResult));
+				writer.flush();
+				writer.close();
+
+			}
+
+		} catch (
+
+		IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
